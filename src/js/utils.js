309fa256c41321f62977f08481037a66e65c22ce
Andrea_Mog <andreajohnbrian@gmail.com>
2026-05-28 18:08:30 +0200

    Aggiornato

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,4 +1,4 @@
-function findIndex(list, callback){
+function findIndex(list, callback){ // Funzione per trovare l'indice di un elemento in una lista che soddisfa una condizione specificata da una callback
     for(var i = 0; i < list.length; i++){
         if(callback(list[i])){
             return i
@@ -9,11 +9,11 @@ function findIndex(list, callback){
     }
 }
 
-function isValidOperation(operation) { // Funzione per validare un'operazione
+function isValidOperation(operation) { // Funzione per verificare se un'operazione è valida, controllando se esiste, se ha una descrizione, se l'importo è maggiore di 0 e se il tipo è valido
     return operation && operation.description && parseFloat(operation.amount) > 0 && typeof OperationTypes[operation.type] !== 'undefined'; // Controlla se l'operazione esiste, se ha una descrizione, se un importo maggiore di 0 e se è un tipo valido
 }
 
-function getWallet() { // Funzione per ottenere il wallet dal localStorage o creare un nuovo wallet se non esiste
+function getWallet() { // Funzione per ottenere il wallet dal localStorage, se non esiste ne crea uno nuovo con bilancio 0 e nessuna operazione
 
     const wallet = localStorage.getItem('wallet'); // Prova a prendere il wallet dal localStorage
 
