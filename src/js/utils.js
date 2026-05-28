8a8b4632f18178f84ca9e2d55493c03face07658
Andrea_Mog <andreajohnbrian@gmail.com>
2026-05-28 17:59:07 +0200

    Spostato le funzioni isValidOperation e getWallet in utils.js

--- src/js/utils.js
+++ src/js/utils.js
@@ -8,3 +8,22 @@ function findIndex(list, callback){
 
     }
 }
+
+function isValidOperation(operation) { // Funzione per validare un'operazione
+    return operation && operation.description && parseFloat(operation.amount) > 0 && typeof OperationTypes[operation.type] !== 'undefined'; // Controlla se l'operazione esiste, se ha una descrizione, se un importo maggiore di 0 e se è un tipo valido
+}
+
+function getWallet() { // Funzione per ottenere il wallet dal localStorage o creare un nuovo wallet se non esiste
+
+    const wallet = localStorage.getItem('wallet'); // Prova a prendere il wallet dal localStorage
+
+    if(!wallet) { // Se non esiste, crea un nuovo wallet con bilancio 0 e nessuna operazione
+        return {
+            balance: 0,
+            operations: []
+        }
+    }
+
+    return JSON.parse(wallet); // Se esiste, restituisci il wallet convertito da JSON
+
+}
